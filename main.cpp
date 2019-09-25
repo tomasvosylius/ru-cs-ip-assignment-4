f05efcad2329cd19dc8d205281a3770922ed6b65
Tomas Vosylius <vslstomas@gmail.com>
2019-09-25 19:26:46 +0200

    Created a function to calculate holy day date given Easter date.

--- main.cpp
+++ main.cpp
@@ -72,19 +72,71 @@ int number_of_days_in_month(int year, Month month)
 /********************************************************************
     Assignment part 2: Holy days based on Easter
 ********************************************************************/
+int calculate_holy_day(int startYear, int startMonth, int startDay, int difference)
+{
+    /*  Function will return base of new holy day date, given start date and difference(in days):
+        Return form: month*31 + days
+
+        So it can be later extracted with:
+        month = base / 31
+        day = (base % 31) + 1
+    */
+    int newMonth    = startMonth,
+        newDay      = startDay;
+    bool adding     = (difference > 0); // If difference is positive, we are adding days. Otherwise, we are subtracting.
+
+    /*  We know whether we are subtracting or adding, so we can invert negative to positive now.
+        We will need for while loop
+    */
+    if(difference < 0) difference *= -1;
+
+    while(difference --> 0)
+    {
+        if(!adding)
+        {
+            /*  If difference is negative, we are subtracting days (from Easter date in this assignment)
+            */
+            newDay--;
+            if(newDay <= 0)
+            {
+                // We subtracted entire month (or what has left of it), so move to (month-1), and start from last day of that month.
+                newMonth -- ;
+                /*  We could check if(newMonth <= 0) and subtract 1 from current year in loop (also would be newMonth = 12)
+                    But we don't have to take care of this, since all holy days are on the same year.
+                    It is also impossible for year to change, because Easter is in spring and max difference is -49 and +49 days.
+                */
+                newDay = number_of_days_in_month(startYear, static_cast<Month>(newMonth)); // Month changed, so does number of days in month.
+            }
+        }
+        else
+        {
+            /*  Difference was positive, so adding = true. Now we are adding days (to Easter date in this assignment)
+            */
+            newDay++;
+            if(newDay > number_of_days_in_month(startYear, static_cast<Month>(newMonth)))
+            {
+                // We went over that month`s days, so move to (month+1), and start from day 1
+                newMonth ++ ;
+                newDay = 1; // Month changed, so does we reset days to 1
+            }
+        }
+    }
+    return (newMonth * 31) + newDay;
+}
+
 
 void show_holy_days ()
 {
-    int inputYear   = 0;
+    int inputYear   = 0,
+        base        = 0;
 
     std::cout << "Input desired year:" << std::endl;
     std::cin >> inputYear;
 
     int easterMonth = easter_month(inputYear),
         easterDay   = easter_day(inputYear),
-        holyMonth   = 0, // We'll use these for calculating holy days
-        holyDay     = 0,
-        diffDays    = 0; // We'll use this to calculate difference
+        holyMonth   = 0, // We'll use these to display holy days
+        holyDay     = 0;
 
     std::cout << "----------------------------------------------------------" << std::endl;
     std::cout << "---------------- HOLY CHRISTIAN DAYS " << inputYear << " ----------------" << std::endl;
@@ -94,82 +146,41 @@ void show_holy_days ()
         Now, as we have easter month and day, we can calculate other holy days with known offsets.
     */
 
-    /**  Carnival
+    /*  Carnival
         We know, that Carnival is 7 weeks before Easter, ending on Tuesday (we know that Easter is always on Sunday)
     */
-    holyMonth   = easterMonth;
-    holyDay     = easterDay;
-    diffDays    = 7 * 7 - 2; /* This is showing how many days we have to subtract from Easter. 7 weeks (49 days), but it ends on Tuesday (-2). */
+    base        = calculate_holy_day(inputYear, easterMonth, easterDay, -47);
+    holyMonth   = base / 31;
+    holyDay     = (base % 31);
 
-    while(diffDays --> 0)
-    {
-        holyDay--;
-        if(holyDay <= 0)
-        {
-            // We subtracted entire month (or what has left of it), so move to (month-1), and start from last day of that (month-1)
-            holyMonth -- ;
-            /*  We could take care of checking if(holyMonth <= 0) and subtract 1 from current year in loop.
-                But we don't have to do this, since all holy days are on the same year. It is also impossible for year to change, because
-                Easter is in spring and max difference is -49 and +49 days.
-            */
-            holyDay = number_of_days_in_month(inputYear, static_cast<Month>(inputYear)); // Month changed, so does number of days in month.
-        }
-    }
     std::cout << "Carnival: " << inputYear << "/" << holyMonth << "/" << holyDay << std::endl;
-    /** Good Friday
+
+    /*  Good Friday
         This is Friday before Easter, so we have to subtract 2 days from Easter day.
     */
-    holyMonth   = easterMonth;
-    holyDay     = easterDay;
-    diffDays    = 2;
-    while(diffDays --> 0)
-    {
-        holyDay--;
-        if(holyDay <= 0)
-        {
-            // We subtracted entire month (or what has left of it), so move to (month-1), and start from last day of that (month-1)
-            holyMonth -- ;
-            holyDay = number_of_days_in_month(inputYear, static_cast<Month>(inputYear)); // Month changed, so does number of days in month.
-        }
-    }
+    base        = calculate_holy_day(inputYear, easterMonth, easterDay, -2);
+    holyMonth   = base / 31;
+    holyDay     = (base % 31);
     std::cout << "Good Friday: " << inputYear << "/" << holyMonth << "/" << holyDay << std::endl;
 
 
-    /** Ascension Day
+    /*  Ascension Day
         This is happening 10 days before Whitsuntide, which is 49 days after Easter. Hence it is on 39th day after Easter.
     */
-    holyMonth   = easterMonth;
-    holyDay     = easterDay;
-    diffDays    = 39;
-    while(diffDays --> 0)
-    {
-        holyDay ++ ;
-        if(holyDay > number_of_days_in_month(inputYear, static_cast<Month>(holyMonth)))
-        {
-            // We went over that month`s days, so move to (month+1), and start from day 1
-            holyMonth ++ ;
-            holyDay = 1; // Month changed, so does we reset days to 1
-        }
-    }
+    base        = calculate_holy_day(inputYear, easterMonth, easterDay, 39);
+    holyMonth   = base / 31;
+    holyDay     = (base % 31);
     std::cout << "Ascension Day: " << inputYear << "/" << holyMonth << "/" << holyDay << std::endl;
 
-    /** Whitsuntide
+    /*  Whitsuntide
         This is happening 49 days after Easter.
     */
-    holyMonth   = easterMonth;
-    holyDay     = easterDay;
-    diffDays    = 49;
-    while(diffDays --> 0)
-    {
-        holyDay ++ ;
-        if(holyDay > number_of_days_in_month(inputYear, static_cast<Month>(holyMonth)))
-        {
-            // We went over that month`s days, so move to (month+1), and start from day 1
-            holyMonth ++ ;
-            holyDay = 1; // Month changed, so does we reset days to 1
-        }
-    }
+    base        = calculate_holy_day(inputYear, easterMonth, easterDay, 49);
+    holyMonth   = base / 31;
+    holyDay     = (base % 31);
     std::cout << "Whitsuntide Day: " << inputYear << "/" << holyMonth << "/" << holyDay << std::endl;
+
+    // Ending print
     std::cout << "----------------------------------------------------------" << std::endl;
 }
 
